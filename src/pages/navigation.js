5fd05bc1ede68c15183f2d10ae23d8919b20b017
Anas Hafsi <AnasHafsii@gmail.com>
2022-09-29 15:15:56 +0200

    Adding NavLinks as a list

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -4,45 +4,68 @@ import Dribbble from "../assets/dribbble.inline.svg";
 import Instagram from "../assets/instagram.inline.svg";
 import Linkedin from "../assets/linkedin.inline.svg";
 
+const internalLinks = [
+    {
+        id: 1,
+        name: "Project",
+        link: "#Project"
+    }, {
+        id: 2,
+        name: "About",
+        link: "#About"
+    }, {
+        id: 3,
+        name: "Resume",
+        link: "#Resume"
+    }, {
+        id: 4,
+        name: "Contact",
+        link: "#Contact"
+    },
+];
+const externalLinks = [
+    {
+        id: 1,
+        name: "Behance",
+        link: "behance.com",
+        icon: <Behance></Behance>
+    }, {
+        id: 2,
+        name: "Instagram",
+        link: "Instagram.com",
+        icon: <Instagram />
+    }, {
+        id: 3,
+        name: "Dribble",
+        link: "Dribble.com",
+        icon: <Dribbble />
+    }, {
+        id: 4,
+        name: "LinkedIn",
+        link: "LinkedIn.com",
+        icon: <Linkedin />
+    },
+];
+
+
+
 function Navigation() {
     return (
         <div className="navigation">
             <div className="row">
                 <div className="inPage">
-                    <div className="element">
-                        <a href="#Project">Project</a>
-                    </div>
-                    <div className="element">
-                        <a href="#About">About</a>
-                    </div>
-                    <div className="element">
-                        <a href="#Resume">Resume</a>
-                    </div>
-                    <div className="element">
-                        <a href="#Contact">Contact</a>
-                    </div>
+                    {internalLinks.map(navItem => (
+                        <div className="element">
+                            <a href={navItem.link}>{navItem.name}</a>
+                        </div>
+                    ))}
                 </div>
                 <div className="external">
-                    <div className="element">
-                        <a>
-                            <Instagram />
-                        </a>
-                    </div>
-                    <div className="element">
-                        <a>
-                            <Linkedin />
-                        </a>
-                    </div>
-                    <div className="element clearfix">
-                        <a className="clearfix">
-                            <Dribbble className="clearfix" />
-                        </a>
-                    </div>
-                    <div className="element">
-                        <a>
-                            <Behance />
-                        </a>
-                    </div>
+                    {externalLinks.map(navItem => (
+                        <div className="element">
+                            <a href={navItem.link}>{navItem.icon}</a>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
